4e73bc33672da55870c3cee20c0428718482635b
DanielSalis <danielsalis98@gmail.com>
2023-02-16 20:24:15 -0300

    feat: add vue rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
     browser: true
   },
   extends: [
+    "plugin:vue/essential",
     "plugin:vue/recommended",
+    "plugin:vue/strongly-recommended",
     "eslint:recommended",
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/script-indent": ["error", 2, { "baseIndent": 1 }]
   },
   globals: {
     $nuxt: true
