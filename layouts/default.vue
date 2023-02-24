a5b15f182a7fa8e3cff18b0d457efe2017bf1d6a
DanielSalis <danielsalis98@gmail.com>
2023-02-24 08:46:36 -0300

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app class="default-layout">
+    <main class="default-layout__content">
+      <Nuxt />
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'DefaultLayout'
+  }
+</script>
+
+<style lang="scss" scoped>
+.default-layout {
+  background-color: #F6F7F8;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.default-layout__content{
+  flex: 1;
+}
+</style>
