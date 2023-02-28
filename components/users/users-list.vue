6a8a36628a554e682dffa4e95671e3394ccc6899
DanielSalis <danielsalis98@gmail.com>
2023-02-28 20:57:42 -0300

    feat: add users-list component

---
+++ components/users/users-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card flat>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :search="search"
+    />
+  </v-card>
+</template>
+<script>
+  export default {
+    name: "UsersList",
+    props: {
+      headers: {
+        required: true,
+        type: Array
+      },
+      users: {
+        required: true,
+        type: Array
+      }
+    },
+    data() {
+      return {
+        search: ''
+      }
+    }
+  }
+</script>
