75e47a62c63d6843f750bf3ee702f488705c85ce
DanielSalis <danielsalis98@gmail.com>
2023-02-27 21:35:20 -0300

    feat: create users admin page

---
+++ pages/admin/users.vue
@@ -0,0 +1,23 @@
+<template>
+  <gContainer class="admin-user-page-container">
+    <h1>Usuários</h1>
+  </gContainer>
+</template>
+
+<script>
+  import gContainer from '~/components/g-container.vue';
+  export default {
+    name: 'AdminUserPage',
+    components: {
+      gContainer
+    },
+    layout: 'admin'
+  }
+</script>
+
+<style lang="scss">
+.admin-user-page-container{
+  width: 100%;
+  margin-top: 76px;
+}
+</style>
