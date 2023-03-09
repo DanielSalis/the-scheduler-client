4b47a5ad32f4030d10574c442c7927455fa3b9a9
DanielSalis <danielsalis98@gmail.com>
2023-03-09 20:42:46 -0300

    feat: create home container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>home</p>
+  <div class="home-page__container">
+    <p>home</p>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,12 @@
 </script>
 
 <style lang="scss">
-
+.home-page__container{
+  max-width: 720px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 24px auto;
+}
 </style>
