b07227365b1d9a3df9d8c0e40002e36307c3a4b6
DanielSalis <danielsalis98@gmail.com>
2023-03-10 12:08:48 -0300

    feat: add dynamic sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <gContainer class="home-page__container">
-    <div>
+    <div
+      v-for="route,index in routeItems"
+      :key="index"
+    >
       <div class="text-h5 ml-3">
-        Gerenciamento
+        {{ route.name }}
       </div>
       <v-row
         class="my-6 mx-0"
         style="width: 100%;"
       >
         <v-col
-          v-for="route, index in routeItems"
-          :key="index"
+          v-for="tool, toolsIndex in route.tools"
+          :key="toolsIndex"
           cols="12"
           sm="12"
           md="6"
@@ -22,9 +25,9 @@
             <v-list-item three-line>
               <v-list-item-content>
                 <v-list-item-title class="text-h6 mb-1">
-                  {{ route.name }}
+                  {{ tool.name }}
                 </v-list-item-title>
-                <v-list-item-subtitle>{{ route.description }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ tool.description }}</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
 
@@ -33,6 +36,7 @@
                 outlined
                 text
                 block
+                @click="redirectToRoute(tool.route)"
               >
                 Acessar
               </v-btn>
@@ -55,18 +59,28 @@
       return {
         routeItems: [
           {
-            name: 'Usuários',
-            description: 'Crie / Edite / Delete / Atualize usuários',
-            route:"/admin/users"
+            name: "Admin",
+            tools: [
+              {
+                name: 'Usuários',
+                description: 'Crie / Edite / Delete / Atualize usuários',
+                route:"/admin/users"
+              },
+              {
+                name: 'Unidades',
+                description: 'Crie / Edite / Delete / Atualize Unidades',
+                route:"/admin/unities"
+              }
+            ]
           },
-          {
-            name: 'Unidades',
-            description: 'Crie / Edite / Delete / Atualize Unidades',
-            route:"/admin/unities"
-          }
         ]
       }
-    }
+    },
+    methods: {
+      redirectToRoute(route){
+        this.$router.push(route)
+      }
+    },
   }
 </script>
 
