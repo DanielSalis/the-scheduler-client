06af3c3834541281701ef6d214fb59554bc04496
DanielSalis <danielsalis98@gmail.com>
2023-03-10 11:17:55 -0300

    feat: dynamic render

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,46 @@
 <template>
   <gContainer class="home-page__container">
     <div class="text-h5 ml-3">
-      Gerenciamento
-    </div>
-    <v-row
-      class="my-6 mx-0"
-      style="width: 100%;"
-    >
-      <v-col
-        cols="12"
-        sm="12"
-        md="6"
-        lg="4"
+      <div class="text-h5 ml-3">
+        Gerenciamento
+      </div>
+      <v-row
+        class="my-6 mx-0"
+        style="width: 100%;"
       >
-        <v-card
-          outlined
+        <v-col
+          v-for="route, index in routeItems"
+          :key="index"
+          cols="12"
+          sm="12"
+          md="6"
+          lg="4"
         >
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6 mb-1">
-                Usuários
-              </v-list-item-title>
-              <v-list-item-subtitle>Crie / Edite / Delete / Atualize usuários</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+          <v-card
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h6 mb-1">
+                  {{ route.name }}
+                </v-list-item-title>
+                <v-list-item-subtitle>{{ route.description }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-          <v-card-actions>
-            <v-btn
-              outlined
-              text
-              block
-            >
-              Acessar
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        sm="12"
-        md="6"
-        lg="4"
-      >
-        <v-card class="home-page__card">
-          teste 2
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        sm="12"
-        md="6"
-        lg="4"
-      >
-        <v-card class="home-page__card">
-          teste 2
-        </v-card>
-      </v-col>
-    </v-row>
+            <v-card-actions>
+              <v-btn
+                outlined
+                text
+                block
+              >
+                Acessar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </gContainer>
 </template>
 
@@ -66,6 +50,22 @@
     name: "HomePage",
     components: {
       gContainer
+    },
+    data() {
+      return {
+        routeItems: [
+          {
+            name: 'Usuários',
+            description: 'Crie / Edite / Delete / Atualize usuários',
+            route:"/admin/users"
+          },
+          {
+            name: 'Unidades',
+            description: 'Crie / Edite / Delete / Atualize Unidades',
+            route:"/admin/unities"
+          }
+        ]
+      }
     }
   }
 </script>
