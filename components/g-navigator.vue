892d06fecc3fa57bfa96459292bd4f88dd2d4e1e
DanielSalis <danielsalis98@gmail.com>
2023-03-10 16:29:15 -0300

    feat: refactor and add dynamic items build

--- components/g-navigator.vue
+++ components/g-navigator.vue
@@ -25,18 +25,15 @@
           v-model="group"
           active-class="primary--text text--accent-4"
         >
-          <v-list-item>
+          <v-list-item
+            v-for="item,index in items"
+            :key="index"
+            @click="$router.push(item.route)"
+          >
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -50,6 +47,23 @@
     data: () => ({
       drawer: false,
       group: null,
+      items: [
+        {
+          icon:'mdi-home',
+          name:'Home',
+          route:'/',
+        },
+        {
+          icon:'mdi-account',
+          name:'Usuários',
+          route:'/admin/users',
+        },
+        {
+          icon:'mdi-exit-to-app',
+          name:'Sair',
+          route:'/login',
+        }
+      ]
     }),
     computed:{
       ...mapGetters("auth", ['getAuthData']),
