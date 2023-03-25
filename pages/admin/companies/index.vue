5d4fc5e87299c835a4505dd5b58b13b14d5f9baf
DanielSalis <danielsalis98@gmail.com>
2023-03-25 08:39:23 -0300

    feat: import global container

--- pages/admin/companies/index.vue
+++ pages/admin/companies/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    Companies
-  </div>
+  <gContainer class="admin-user-page-container">
+    Teste
+  </gContainer>
 </template>
 
 <script>
+  import gContainer from '~/components/g-container.vue';
   export default {
-    name: "AdminCompaniesPage"
+    name: "AdminCompaniesPage",
+    components: {
+      gContainer
+    }
   }
 </script>
 
