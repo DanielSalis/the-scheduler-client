0af9d7222b0c2d0469f1f336dd91319fa8aebb23
DanielSalis <danielsalis98@gmail.com>
2023-03-25 09:39:55 -0300

    feat: add tabs

--- pages/admin/companies/index.vue
+++ pages/admin/companies/index.vue
@@ -1,6 +1,34 @@
 <template>
-  <gContainer class="admin-user-page-container">
-    Teste
+  <gContainer class="admin-companies-page-container">
+    <v-card>
+      <v-toolbar
+        flat
+        color="white"
+      >
+        <v-toolbar-title>Redes Hospitalares</v-toolbar-title>
+      </v-toolbar>
+      <v-tabs>
+        <v-tab>
+          <v-icon left>
+            mdi-table
+          </v-icon>
+          Listar
+        </v-tab>
+        <v-tab>
+          <v-icon left>
+            mdi-plus
+          </v-icon>
+          Adicionar
+        </v-tab>
+
+        <v-tab-item>
+          <p>Listar</p>
+        </v-tab-item>
+        <v-tab-item>
+          <p>Aba de adicionar</p>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
   </gContainer>
 </template>
 
@@ -15,5 +43,8 @@
 </script>
 
 <style lang="scss" scoped>
-
+.admin-companies-page-container{
+  width: 100%;
+  margin-top: 76px;
+}
 </style>
