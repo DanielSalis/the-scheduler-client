f7bd439cc4abb81305ece6e7bc26bd8a247eb98b
DanielSalis <danielsalis98@gmail.com>
2023-03-31 15:46:52 -0300

    feat: create add beds component

---
+++ components/beds/beds-add.vue
@@ -0,0 +1,100 @@
+<template lang="">
+  <v-card flat>
+    <v-card-text>
+      <v-form
+        ref="form"
+        lazy-validation
+        @submit.prevent="add"
+      >
+        <v-text-field
+          v-model="bedInfo.name"
+          :rules="nameRules"
+          solo
+          label="Nome do Leito"
+          required
+        />
+
+        <v-select
+          v-model="bedInfo.classification_id"
+          :items="classifications"
+          :rules="classificationsRule"
+          item-text="name"
+          item-value="id"
+          label="Classificação"
+          solo
+        />
+
+        <v-select
+          v-model="bedInfo.unity_id"
+          :items="unities"
+          :rules="unitiesRule"
+          item-text="name"
+          item-value="id"
+          label="Unidades"
+          solo
+        />
+
+        <v-btn
+          block
+          color="primary"
+          class="mr-4"
+          validate-on="submit"
+          type="submit"
+        >
+          Adicionar
+        </v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "BedsAdd",
+    props: {
+      unities: {
+        required: true,
+        type: Array
+      },
+      classifications: {
+        required: true,
+        type: Array
+      },
+    },
+    data() {
+      return {
+        availableHospitals: null,
+        bedInfo: {
+          name: '',
+          unity_id: '',
+          classification_id: ''
+        },
+        nameRules: [
+          v => !!v || 'Nome é necessário',
+          v => v.length >= 10 || 'Nome precisa ter no mínimo 8 caracteres',
+        ],
+        classificationsRule: [
+          v => !!v || 'Classificação é necessária',
+        ],
+        unitiesRule: [
+          v => !!v || 'Unidad é necessária',
+        ],
+      }
+    },
+    methods: {
+      async add(){
+        await this.$axios.post("/bed/create", this.bedInfo).then((response)=>{
+          console.log(response);
+          alert("Unidade criada com sucesso!")
+          this.$router.go('/admin/beds')
+        }).catch(error=>{
+          alert(`Erro ao criar unidade: ${error.response.data.error}`)
+        })
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
