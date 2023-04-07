2e47f8774833c93e00430a8635c186442b3e5f9f
DanielSalis <danielsalis98@gmail.com>
2023-04-07 16:12:50 -0300

    feat: create schedule page

---
+++ pages/schedule.vue
@@ -0,0 +1,20 @@
+<template>
+  <GContainer class="schedule-page__container">
+    Dimensionamento teste
+  </GContainer>
+</template>
+
+<script>
+  import GContainer from '~/components/g-container.vue';
+
+  export default {
+    name: "SchedulePage",
+    components: { GContainer }
+  }
+</script>
+
+<style lang="scss" ts>
+.schedule-page__container{
+  margin-top: 40px;
+}
+</style>
