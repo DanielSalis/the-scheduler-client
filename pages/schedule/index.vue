1655cb2c47c0ea5e7dd2d615c3dfb4f84e5feea3
DanielSalis <danielsalis98@gmail.com>
2023-04-09 14:33:01 -0300

    fix: remove comment section

--- pages/schedule/index.vue
+++ pages/schedule/index.vue
@@ -17,31 +17,6 @@
 
           <v-divider v-if="index < stepps.length - 1" />
         </Fragment>
-
-        <!-- <v-stepper-step
-          :complete="e1 > 1"
-          step="1"
-        >
-          Name of step 1
-        </v-stepper-step>
-
-        <v-divider />
-
-        <v-stepper-step
-          :complete="e1 > 2"
-          step="2"
-        >
-          Name of step 2
-        </v-stepper-step>
-
-        <v-divider />
-
-        <v-stepper-step
-          step="3"
-          :complete="e1 > 3"
-        >
-          Name of step 3
-        </v-stepper-step> -->
       </v-stepper-header>
 
       <v-stepper-items>
